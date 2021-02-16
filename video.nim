29dd2f49d2be179cd1a6326a146fb9768d009cf4
yovel <ykey-cohen@emeryweiner.org>
2021-02-15 22:35:22 -0600

    mpgggggggggggggggggggggggggg

--- video.nim
+++ video.nim
@@ -1,6 +1,10 @@
 import mpeg
 import os
 
-const vp = "C:\\Users\\yovel\\Desktop\\sample.mov"
+const vp = "C:\\Users\\yovel\\Desktop\\sample.mpg"
 
 var player = plm_create_with_filename(vp)
+
+var frame = player.plm_decode_video()
+
+echo frame[]
